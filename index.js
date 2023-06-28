276e0e072fd6866cdb1748632b5d05704ab19523
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-06-28 08:18:51 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-// Add your code here
+// // Add your code here
+// // fetch(`http://localhost:3000/dogs`)
+// // .then((response=> response.json))
+// // .then((data=>
+// //     console.log (data)))
+//   const formData=  {
+//     dogName:"Byron",
+//     dogBreed:"Poodle",
+//     dogName:"Vicky",
+//     dogBreed:"Labrador"
+// } 
+// const configurationObject= {
+// method: "POST",
+// headers: {
+//     "Content-Type":"application/json",
+//     Accept:"application/json",
+// },
+// body:JSON.stringify(formData),
+// };
+// fetch("http://localhost:3000/dogs",configurationObject)
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function(object) {
+//     console.log(object);
+// });
+
+const formData= {
+    name:"Jason Statham",
+    email:"jasonstatham@gmail.com"
+}
+const submitData= {
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json",
+        Accept:"application/json"
+    },
+    body: JSON.stringify(formData)
+}
+fetch("http://localhost:3000/users",submitData)
+.then(function(response){
+    return response.json()
+})
+    .then(function(object){
+        console.log(object);
+    })
+    .catch(function(error){
+        alert("Beware of the error!");
+        console.log(error.message);
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
